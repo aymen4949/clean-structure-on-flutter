5fc65177adcff5ffa7feeff109ff1d589773e807
aymen4949 <aymen@mohastech.com>
2023-01-18 08:18:03 +0300

    change on usecase

--- md/lib/core/usecases/usecase.dart
+++ md/lib/core/usecases/usecase.dart
@@ -5,9 +5,7 @@ import 'package:md/core/error/failures.dart';
 abstract class UseCase<Type, Params> {
   Future<Either<Failures, Type>?> execute(Params params);
 }
-class NoParams extends Equatable {
-  @override
-  // TODO: implement props
-  List<Object?> get props => throw UnimplementedError();
 
+class NoParams extends Equitable {
+  
 }
